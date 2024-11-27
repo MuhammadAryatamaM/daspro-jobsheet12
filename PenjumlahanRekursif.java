93e92349ee5c01fb44b1fb71e93fc63c3fe29f71
MuhammadAryatamaM <muhammad.aryatama2006@gmail.com>
2024-11-27 11:32:43 +0700

    program menjumlahkan seluruh bilangan dari n sampai 1

--- PenjumlahanRekursif.java
+++ PenjumlahanRekursif.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 public class PenjumlahanRekursif {
-    static int jumlah(int n) {
-        if (n == 1) {
+    static int jumlah(int n, int loop) {
+        if (loop == n) {
             System.out.print(n);
             return n;
         } else {
-            System.out.printf("%d + ", n);
-            return (n + jumlah(n-1));
+            System.out.printf("%d + ", loop);
+            return (loop + jumlah(n, loop+1));
         }
     }
     public static void main(String[] args) {
@@ -15,7 +15,7 @@ public class PenjumlahanRekursif {
         System.out.print("Beri satu angka: ");
         int angka = sc.nextInt();
 
-        int hasil = jumlah(angka);
+        int hasil = jumlah(angka, 1);
         System.out.printf(" = %d",  hasil);
     }
 }
